bbce3bc65ede4939a4de131d0fd8ea43cc053971
audhalbritter <me@audhalbritter.com>
2022-06-19 18:28:30 +0200

    render qmd

--- make_book.R
+++ make_book.R
@@ -1,4 +1,8 @@
-# make book
+# make markdown book
 withr::with_dir("webpage/", {
   bookdown::render_book('index.Rmd', 'bookdown::bs4_book',  output_dir = "../docs/")
 })
+
+
+# Quarto book
+quarto::quarto_render("quarto_webpage")
