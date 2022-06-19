e345bb1502a212931111f4e8446ee42abb8746b2
audhalbritter <me@audhalbritter.com>
2022-06-19 18:51:27 +0200

    fix make book file

--- make_book.R
+++ make_book.R
@@ -1,8 +1,8 @@
-# make markdown book
-withr::with_dir("webpage/", {
-  bookdown::render_book('index.Rmd', 'bookdown::bs4_book',  output_dir = "../docs/")
-})
-
-
-# Quarto book
+# make quarto book
 quarto::quarto_render("quarto_webpage")
+
+
+# withr::with_dir("webpage/", {
+#   bookdown::render_book('index.Rmd', 'bookdown::bs4_book',  output_dir = "../docs/")
+# })
+
